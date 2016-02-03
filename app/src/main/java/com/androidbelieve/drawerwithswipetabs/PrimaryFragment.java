2c045746cfca911b4f65f734fc17d72b5130d029
Muhammad Golam Mahmud Rafi <rafi.aust@live.com>
2016-02-04 00:57:50 +0600

    Recylerview Implemented
    Done

--- app/src/main/java/com/androidbelieve/drawerwithswipetabs/PrimaryFragment.java
+++ app/src/main/java/com/androidbelieve/drawerwithswipetabs/PrimaryFragment.java
@@ -3,6 +3,8 @@ package com.androidbelieve.drawerwithswipetabs;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,10 +13,21 @@ import android.view.ViewGroup;
  * Created by Ratan on 7/29/2015.
  */
 public class PrimaryFragment extends Fragment {
-
+    RecyclerView recyclerView;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.primary_layout,null);
+
+
+        View view = inflater.inflate(R.layout.primary_layout,null);
+
+        recyclerView = (RecyclerView) view.findViewById(R.id.my_recycler_view);
+
+        RecyclerAdapter adapter = new RecyclerAdapter(getActivity());
+        recyclerView.setAdapter(adapter);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        return view;
     }
 }
